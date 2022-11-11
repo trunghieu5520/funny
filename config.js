868352ee70101f22a061876231df3620e825076a
Nguyễn Trung Hieu <78343616+trunghieu5520@users.noreply.github.com>
2022-11-11 15:36:58 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Chào cậu!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'Click vào đây',
+    title: 'Bạn sẽ tặng tôi một chiếc MAC BOOK PRO 2022? 😙',
+    desc: 'Hoặc iPhone 14 PROMAX 1TB nhé ',
+    btnYes: 'Tớ đồng ý <3',
+    btnNo: 'Mai mua cho',
+    question:'Tại sao câu lại mua cho mình <3',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Vì mình thừa tiền, vì cậu rất đẹp trai <33333333',
+    mess: 'hiii yêu cậu',
+    messDesc: 'Nhớ mua luôn nhé :>',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/hieu.nguyen.5520/'
+}
